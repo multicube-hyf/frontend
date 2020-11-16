4c7ade54e65c52459af4ae66610062ceda542896
Katie <katie_lisabeth@yahoo.com>
2020-11-16 11:32:09 +0100

    empty pages for header

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -5,7 +5,7 @@ import background from '../components/images/background.png'
 function Home() {
     return (
         <div className="Home">
-        <img className="backgroundStart" src ={background} alt="people with phones"/>
+        <img className="backgroundHome" src ={background} alt="people with phones"/>
         <h1>Start</h1>
         </div>
     )
