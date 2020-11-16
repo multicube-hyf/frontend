76d049fee97fa6033ed0c439bc54d4344e749269
Katie <katie_lisabeth@yahoo.com>
2020-11-16 10:29:32 +0100

    routes for homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./screens/Home";
+
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
+    <Switch>
+    <Route exact path="/" component={Home} />
+    </Switch>
+    </BrowserRouter>
      
     </div>
   );
