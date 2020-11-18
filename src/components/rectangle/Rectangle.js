e60eb2557b0cdf204246e2d648acaa869d167068
rui hildt <ruihildt@protonmail.com>
2020-11-18 16:30:56 +0100

    Add dynamic numbers highlight

--- src/components/rectangle/Rectangle.js
+++ src/components/rectangle/Rectangle.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import Cube from '../cube/Cube';
-import './rectangle.css'
+import './rectangle.css';
 
 Rectangle.defaultProps = {
-    number: 10,
+	number: 10,
 	isHighlighted: false,
 };
 
-function Rectangle({number, selectedNumber, highlightColor, isHighlighted }) {
-	// Generate an array with a list of number from 1 to number
-	const numbers = [...Array(number).keys()];
+function Rectangle({ number, selectedNumber, highlightColor, isHighlighted }) {
+	// Generate arrays with a list of numbers
+	
+	// Highlighted numbers
+	let highlightedNumbers = [...Array(selectedNumber).keys()];
+	
+	// Regular numbers
+	const remainingNumbers = number - selectedNumber
+	let regularNumbers = [...Array(remainingNumbers).keys()];
 
 	return (
-		<div 
-		   className="rectangle"
-		>
-			{numbers.map((number) => (
+		<div className='rectangle'>
+			{highlightedNumbers.map((number) => (
+				<Cube
+					key={number}
+					backgroundColor={isHighlighted ? highlightColor : '#000000'}
+				/>
+			))}
+			{regularNumbers.map((number) => (
 				<Cube
 					key={number}
 					backgroundColor={isHighlighted ? highlightColor : '#858585'}
--- src/screens/Help.js
+++ src/screens/Help.js
@@ -1,17 +1,16 @@
-import React, {useState}from 'react';
+import React, { useState } from 'react';
+
 import Pyramid from '../components/pyramid/Pyramid';
 import Rectangle from '../components/rectangle/Rectangle';
 
 function Help() {
-    const [selectedNumber, setSelectedNumber] = useState();
-   
-
+	const [selectedNumber, setSelectedNumber] = useState(1);
 
 	return (
 		<div>
-            <Pyramid setSelectedNumber={setSelectedNumber}/>
-            <h3>{selectedNumber}</h3>
-            <Rectangle selectedNumber={selectedNumber}/>
+			<Pyramid setSelectedNumber={setSelectedNumber} />
+			<h3>{selectedNumber}</h3>
+			<Rectangle selectedNumber={selectedNumber} />
 		</div>
 	);
 }
