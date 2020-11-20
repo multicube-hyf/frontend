17f584243778993f58abffe6ad389186335e2751
Katie <katie_lisabeth@yahoo.com>
2020-11-20 21:57:42 +0100

    Structure footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Help from "../components/help/Help";
+import ArrowForwardSharpIcon from '@material-ui/icons/ArrowForwardSharp';
+import { Fab } from "@material-ui/core";
+
+
+
+
 
 function Footer() {
   return (
     <div>
-      <Navbar className="footer fixed-bottom navbar-expand-lg">
-        <Link to="/help">Help</Link>
+      <Navbar className="footer fixed-bottom justify-content-between">
+      <Fab color="secondary" aria-label="edit">
+       <Link to="/help"><Help/></Link>
+       </Fab>
+       <Nav>
+       <Nav.Link><Button className="">Get Started</Button></Nav.Link>
+       </Nav>
+       <Fab color="secondary" aria-label="edit">
+       <Nav.Link href="/"><ArrowForwardSharpIcon/></Nav.Link>
+       </Fab>
+       
+       
       </Navbar>
     </div>
   );
 }
 
 export default Footer;
+
